c27f5a5364e877cafff740b12086569c9cd82a7c
Shafi Abdullah <abdullah.shafi99@gmail.com>
2023-11-03 19:22:10 +0600

    files upload

---
+++ Projectaaa/app/src/main/java/com/example/project101/Transaction_history.java
@@ -0,0 +1,14 @@
+package com.example.project101;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Transaction_history extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transaction_history);
+    }
+}
